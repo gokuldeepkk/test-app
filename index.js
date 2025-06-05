f1f62a45f6bbe46c41d54683086f20ca0901a6f7
Gokul Deep K K <gokuldeep41@gmail.com>
2025-06-05 17:33:25 +0530

    adding .json extension

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.get("/.well-known/assetlinks", (req, res) => {
+app.get("/.well-known/assetlinks.json", (req, res) => {
   res.sendFile(path.join(__dirname, "/well-known/assetlinks.json"));
 });
 
