7c6d4089382afc40be039d2cb1480e40ab5a7899
Gokul Deep K K <gokuldeep41@gmail.com>
2025-06-05 15:33:18 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/.well-known/assetlinks", (req, res) => {
+  res.sendFile(path.join(__dirname, "/well-known/assetlinks.json"));
+});
+
+app.get("/.well-known/apple-app-site-association", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/well-known/apple-app-site-association.json")
+  );
+});
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
